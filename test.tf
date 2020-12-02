c8ce7b99ac924b4dcdebb690c281e9860341757c
gautham990 <gautham990@gmail.com>
2020-12-02 09:23:05 +0800

    first commit

---
+++ test.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-southeast-1"
+}
+resource "aws_instance" "web1" {
+  ami           = "ami-03faaf9cde2b38e9f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "webserver1"
+  }
+}
+resource "aws_instance" "web2" {
+  ami           = "ami-03faaf9cde2b38e9f"
+  instance_type = "t2.micro"
+  tags - {
+    Name = "webserver2"
+}
